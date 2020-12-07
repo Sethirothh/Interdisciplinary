1efedc6f6f1b7c86af1cba7d46ef855d2ffe2968
Jesper <jespergyldenlve@gmail.com>
2020-12-07 10:51:10 +0100

    Validation Styling og fucking fed

--- Interdisciplinary/Interdisciplinary/Models/Food.cs
+++ Interdisciplinary/Interdisciplinary/Models/Food.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 
 namespace Interdisciplinary.Models
 {
